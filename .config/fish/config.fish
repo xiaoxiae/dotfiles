3b2b669d84b9515435d56c195908ec3d16cb04fa
xiaoxiae <tomas.slama.131@gmail.com>
2020-03-29 10:56:05 +0200

    prevent nested ranger instances

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,11 @@
 set fish_greeting
+
+# prevent nested ranger instances
+# https://wiki.archlinux.org/index.php/Ranger
+function ranger
+	if [ -z "$RANGER_LEVEL" ]
+		/usr/bin/ranger "$argv"
+	else
+		exit
+	end
+end
