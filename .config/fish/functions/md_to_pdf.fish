1bb52ed52cae9586942a297f673d06efcb48d4a9
xiaoxiae <tomas.slama.131@gmail.com>
2020-04-18 18:42:04 +0200

    fix incorrect md_to_pdf path

--- .config/fish/functions/md_to_pdf.fish
+++ .config/fish/functions/md_to_pdf.fish
@@ -1,3 +1,3 @@
 function md_to_pdf
-	~/Documents/Education/School/MatFyz/Studium/Skripty/md_to_pdf/md_to_pdf/md_to_pdf $argv
+	~/Documents/Education/School/MatFyz/Studium/Skripty/md_to_pdf/md_to_pdf $argv
 end
