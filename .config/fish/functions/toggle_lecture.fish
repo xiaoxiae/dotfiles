5fa465c780194be64af8c5b358038c1017f67f16
xiaoxiae <tomas.slama.131@gmail.com>
2020-07-15 17:06:18 +0200

    lecture script setup

---
+++ .config/fish/functions/toggle_lecture.fish
@@ -0,0 +1,11 @@
+function toggle_lecture
+	# toggle vimrc
+	mv ~/.vimrc ~/.vimrc_
+	mv ~/.vimrc_lecture ~/.vimrc
+	mv ~/.vimrc_ ~/.vimrc_lecture
+
+	# toggle terminal profile 
+	mv ~/.terminal_profile ~/.terminal_profile_
+	mv ~/.terminal_profile_lecture ~/.terminal_profile
+	mv ~/.terminal_profile_ ~/.terminal_profile_lecture
+end
